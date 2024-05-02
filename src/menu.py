30e5c170f6d1fa5bf8e9ab901c4445e1fa500e96
Emily Goldstein <egoldst8@binghamton.edu>
2024-05-02 00:00:01 -0400

    efvdsvsvdsvdsv

--- src/menu.py
+++ src/menu.py
@@ -6,15 +6,15 @@ screen = pygame.display.set_mode((900, 700))
 background1 = pygame.image.load("assets/dungeon.jpeg")
 background1 = pygame.transform.scale(background1, (900, 700))
 
-start_button = pygame.Rect(325, 560, 200, 50)
+start_button = pygame.Rect(325, 570, 200, 50)
 font = pygame.font.Font(None, 36)
 text = font.render("Start", True, (0, 0, 0))
 
 font2 = pygame.font.Font(None, 150)
-text2 = font2.render("Escape", True, (255, 255, 255))
+text2 = font2.render("ESCAPE", True, (255, 255, 255))
 
-font3 = pygame.font.Font(None, 47)
-text3 = font3.render("The Dungeon", True, (255, 255, 255))
+font3 = pygame.font.Font(None, 37)
+text3 = font3.render("Enter at your own risk.", True, (255, 255, 255))
 
 run = True
 while run:
@@ -25,8 +25,8 @@ while run:
             screen.blit(background1, (0, 0))
             pygame.draw.rect(screen, (115, 147, 179), start_button)
             screen.blit(text, (start_button.x + 66, start_button.y + 12))
-            screen.blit(text2, (415, 220))
-            screen.blit(text3, (430, 330))
+            screen.blit(text2, (215, 75))
+            screen.blit(text3, (289, 182))
 
         # Update the display
         pygame.display.flip()
